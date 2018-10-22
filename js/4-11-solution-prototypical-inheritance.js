fa427d38e84186484f62ecbe5e01e1cbdb4e6213
herrifransisca <herrifransisca@gmail.com>
2018-10-22 18:45:36 +0700

    10- Exercise- Prototypical Inheritance

--- js/4-11-solution-prototypical-inheritance.js
+++ js/4-11-solution-prototypical-inheritance.js
@@ -1,4 +1,3 @@
-// Mosh's code
 function HtmlElement() {
   this.click = function() {
     console.log('clicked');
@@ -19,49 +18,18 @@ function HtmlSelectElement(items = []) {
   this.removeItem = function(item) {
     this.item.splice(this.items.indexOf(item), 1);
   };
+
+  this.render = function() {
+    const options = this.items
+      .map(value => `<option>${value}</option>`)
+      .join('');
+    console.log(this.items);
+    console.log(options);
+  };
 }
 
 HtmlSelectElement.prototype = new HtmlElement();
 HtmlSelectElement.prototype.constructor = HtmlSelectElement;
 
-// // Herri's code
-// function extend() {
-//   Child.prototype = Object.create(Parent.prototype);
-//   Child.prototype.constructor = Child;
-// }
-
-// function HtmlElement() {
-//   this.click = function() {
-//     console.log('clicked');
-//   };
-// }
-
-// HtmlElement.prototype.focus = function() {
-//   console.log('focused');
-// };
-
-// const e = new HtmlElement();
-// e.click();
-// e.focus();
-
-// function HtmlSelectElement(...items) {
-//   // HtmlElement.call(this);
-
-//   this.items = items;
-
-//   this.addItem = function(item) {
-//     this.items.push(item);
-//   };
-
-//   this.removeItem = function(item) {
-//     const position = this.items.indexOf(item);
-//     this.items.splice(position, 1);
-//   };
-// }
-
-// // extend(HtmlSelectElement, HtmlElement);
-// HtmlSelectElement.prototype = Object.create(e);
-// HtmlSelectElement.prototype.constructor = HtmlSelectElement;
-
-// const s = new HtmlSelectElement();
-// console.log(s);
+const s = new HtmlSelectElement([1, 2, 3]);
+console.log('test');
