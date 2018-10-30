7cec4f4e68f911d600a69757e99a7927d6196f29
herrifransisca <herrifransisca@gmail.com>
2018-10-30 16:17:23 +0700

    5- 1- ES6 Classes

---
+++ js/5-01-es6-classes.js
@@ -0,0 +1,25 @@
+// function Circle(radius) {
+//   this.radius = radius;
+
+//   this.draw = function() {
+//     console.log('draw');
+//   };
+// }
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+
+    this.move = function() {
+      console.log('move');
+    };
+  }
+
+  draw() {
+    console.log('draw');
+  }
+}
+
+const circle = new Circle(1);
+console.log(circle);
+console.log(typeof Circle); // "function"
